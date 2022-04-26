3a2d5b3c844c437d9dce141a0a11d996316d50a5
TROY-PC <TROY-PC@TROY-PC>
2022-04-26 21:49:43 +0800

    Add project files.

---
+++ PDC03_MOD02/PDC03_MOD02/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PDC03_MOD02
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void OnNextPage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity1());
+        }
+        private async void OnActivity2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity2());
+        }
+        private async void OnActivity3(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity3());
+        }
+        private async void OnActivity4(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity4());
+        }
+    }
+}
---
+++ PDC03_MOD02/PDC03_MOD02/activity2.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC03_MOD02
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class activity2 : ContentPage
+    {
+        public activity2()
+        {
+            InitializeComponent();
+        }
+
+       
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            Button button = (Button)sender;
+            await DisplayAlert("Clicked!",
+                "The button '" + button.Text + "' has been clicked", "OK");
+
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            valuelabel.Text = args.NewValue.ToString("F3");
+        }
+    }
+}
